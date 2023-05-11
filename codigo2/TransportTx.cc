89a92912971a1d5c58de49694e75c8c1adc6dcb7
Alejo_Corral <alejo.corral@mi.unc.edu.ar>
2023-05-11 17:38:19 -0300

    comienza con la implementacion de las clases TransportTx y Rx

---
+++ codigo2/TransportTx.cc
@@ -0,0 +1,80 @@
+#ifndef TX
+#define TX
+
+#include <string.h>
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+class TransportTx: public cSimpleModule {
+private:
+    cQueue buffer;
+    cMessage *endServiceEvent;
+    simtime_t serviceTime;
+    cOutVector bufferSizeVector;
+    cOutVector packetDropVector;
+    cOutVector out_vector;
+public:
+    TransportTx();
+    virtual ~TransportTx();
+protected:
+    virtual void initialize();
+    virtual void finish();
+    virtual void handleMessage(cMessage *msg);
+};
+
+Define_Module(TransportTx);
+
+TransportTx::TransportTx() {
+    endServiceEvent = NULL;
+}
+
+TransportTx::~TransportTx() {
+    cancelAndDelete(endServiceEvent);
+}
+
+void TransportTx::initialize() {
+    buffer.setName("buffer");
+    endServiceEvent = new cMessage("endService");
+    
+}
+
+void TransportTx::finish() {
+}
+
+void TransportTx::handleMessage(cMessage *msg) {
+
+    // if msg is signaling an endServiceEvent
+    if (msg == endServiceEvent) {
+        // if packet in buffer, send next one
+        if (!buffer.isEmpty()) {
+            // dequeue packet
+            cPacket *pkt = (cPacket*) buffer.pop();
+            // send packet
+            send(pkt, "out");
+            // start new service
+            serviceTime = pkt->getDuration();
+            scheduleAt(simTime() + serviceTime, endServiceEvent);
+        }
+    } else { // if msg is a data packet
+        // check buffer limit
+        if (buffer.getLength() >= par("bufferSize").intValue()){
+            // drop the packet
+            delete msg;
+            this->bubble("packet dropped");
+            packetDropVector.record(1);
+        }
+        else{
+            // enqueue the packet
+            buffer.insert(msg);
+            bufferSizeVector.record(buffer.getLength());
+            // if the server is idle
+            if (!endServiceEvent->isScheduled()) {
+                // start the service
+                scheduleAt(simTime(), endServiceEvent);
+            }
+        }
+    }
+}
+
+#endif /* QUEUE */
